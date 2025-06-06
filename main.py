ae45fdd0131771ead0ef1411fa73712a92dfba5e
Ms Pell Ted <misspellted@rouge-truth>
2025-06-05 19:20:09 -0500

    Tests one of each of the other FIT files from Garmin Forerunner 35 dump

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 
 from config import FR35_DUMPS_DIR
-
 from hearty.files.fit import FitFile
+
+# fit_file_path = f"{FR35_DUMPS_DIR}/2025-05-24_23-14/DEVICE.FIT" # No errors reading. ^_^
+# fit_file_path = f"{FR35_DUMPS_DIR}/2025-05-24_23-14/ACTIVITY/F3C94745.FIT" # No errors reading. ^_^
+# fit_file_path = f"{FR35_DUMPS_DIR}/2025-05-24_23-14/MONITOR/F5A00000.FIT" # No errors reading. ^_^
+# fit_file_path = f"{FR35_DUMPS_DIR}/2025-05-24_23-14/RECORDS/RECORDS.FIT" # No errors reading. ^_^
+# fit_file_path = f"{FR35_DUMPS_DIR}/2025-05-24_23-14/SETTINGS/SETTINGS.FIT" # No errors reading. ^_^
+fit_file_path = f"{FR35_DUMPS_DIR}/2025-05-24_23-14/SPORTS/0RRUN.FIT" # No errors reading. ^_^
 
 ff = FitFile()
 
-record_count = ff.read_from_file(fit_file_path=f"{FR35_DUMPS_DIR}/2025-05-24_23-14/DEVICE.FIT")
+record_count = ff.read_from_file(fit_file_path=fit_file_path)
+
 print(f"Read {record_count} record(s)")
